702199f7e5bc69fb42c8abe19844445dbd6eace6
Haojin Li <hl420@duke.edu>
2023-02-08 13:56:55 -0500

    Try out auto night-mode toggle

--- themes/icarus/source/js/night.js
+++ themes/icarus/source/js/night.js
@@ -5,8 +5,8 @@
      * https://www.imaegoo.com/
         */
 
-      var isNight = localStorage.getItem('night');
-      var nightNav;
+    var isNight = localStorage.getItem('night');
+    var nightNav;
 
     function applyNight(value) {
         if (value.toString() === 'true') {
@@ -18,21 +18,33 @@
         }
     }
 
-    function findNightNav() {
-        nightNav = document.getElementById('night-nav');
-        if (!nightNav) {
-            setTimeout(findNightNav, 100);
-        } else {
-            nightNav.addEventListener('click', switchNight);
-        }
+    // Auto night toggle based on local time
+    let hour = new Date().getHours();
+    console.log("Local hour: " + hour)
+
+    if (hour >= 18 || hour <= 6) {
+        applyNight('true')
+        console.log("Auto night")
+    } else {
+        applyNight('true')
+        console.log("Auto day")
     }
 
-    function switchNight() {
-        isNight = isNight ? isNight.toString() !== 'true' : true;
-        applyNight(isNight);
-        localStorage.setItem('night', isNight);
-    }
+    // function findNightNav() {
+    //     nightNav = document.getElementById('night-nav');
+    //     if (!nightNav) {
+    //         setTimeout(findNightNav, 100);
+    //     } else {
+    //         nightNav.addEventListener('click', switchNight);
+    //     }
+    // }
 
-    findNightNav();
-    isNight && applyNight(isNight);
+    // function switchNight() {
+    //     isNight = isNight ? isNight.toString() !== 'true' : true;
+    //     applyNight(isNight);
+    //     localStorage.setItem('night', isNight);
+    // }
+
+    // findNightNav();
+    // isNight && applyNight(isNight);
   }());
